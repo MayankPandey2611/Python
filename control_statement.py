cd6e258bc1a9f472ada7d7cc63744f55ea0895af
MayankPandey2611 <kitupnd@gmail.com>
2025-05-07 16:40:26 +0530

    if else questions

--- control_statement.py
+++ control_statement.py
@@ -1,2 +1,84 @@
-# PALINDROME 
+# ----------------------------------------------CONTROL STATEMENTS-------------------------------------------------------------#
+
+# 1. CONDITIONAL STATEMENT =>
+#  IF STATEMENT
+#  IF-ELSE STATEMENT
+#  IF-ELIF STATEMENT
+#  IF-ELIF-ELSE STATEMENT
+
+# 2. ITERATIVE / LOOPING STATEMENT =>
+# WHILE
+# FOR
+
+
+# 3. TRANSFORM STATEMENT =>
+# CONTINUE
+# BREAK
+# PASS
+
+# ------------------------------------------------SYNTAX--------------------------------------------------------------------------#
+
+# IF SYNTAX
+
+# n = int(input("enter a number: "))
+# if n%2==0:
+#     print(f'given number {n} is even')
+   
+
+
+
+# IF-ELSE SYNTAX
+
+# n = int(input("enter a number: "))
+# if n%2==0:
+#     print(f'given number {n} is even')
+# else:
+#     print(f'given number {n} is odd')     
+    
+
+# IF-ELIF-ELSE SYNTAX
+
+# a,b,c=int(input("enter a: ")),int(input("enter b: ")),int(input("enter c: "))
+
+
+# if(a>b and a>c):
+#     print(f'{a} is a greater number')
+# elif(b>a and b>c):
+#     print(f'{b} is a greater number')  
+# elif(a==b and a==c):
+#     print(f'all {a,b,c} are same')    
+# else:
+#     print(f'{c} is a greater number')      
+
+
+# year = int(input("enter the year: "))
+
+# if(year%4==0 and year%100!=0 or year%400==0):
+#     print(f'given year {year} is a leap year.')   
+# else:
+#     print(f'given year {year} is not a leap year.')    
+
+
+
+# h,e,m = float(input("enter marks of hindi: ")),float(input("enter marks of english: ")),float(input("enter marks of maths: "))
+
+# if(h>=0 and h<=100 and e>=0 and e<=100 and m>=0 and m<=100):
+    
+#     avg = (h+e+m)/3
+
+#     if(avg>=60):
+#         print(f'you got A grade.')
+
+#     elif(avg>=45 and avg<=59):
+#         print(f'you got B grade.')
+
+#     elif(avg>=35 and avg<=44):
+#         print(f'you got C grade.')
+
+#     else:
+#         print(f'you are falied.') 
+
+# else:
+#     print(f'entered marks are invalid.')
+
 
