656517be5c60a273025a3f3c4138977cb0cd2719
MayankPandey2611 <kitupnd@gmail.com>
2025-05-18 11:41:08 +0530

    questions

--- functions.py
+++ functions.py
@@ -82,23 +82,98 @@
 # QUES 6. FACTORIAL OF A NUMBER.
 
 # def fact(n):
+#     ans=1
 #     for i in range(1,n+1):
-#         ans=n*(n-1)
+#         ans=ans*i
 #     return ans
-
-
-        
 
 # n=int(input())
 # ans = fact(n)
 # print(ans)
 
 
-def checkleap(year):
-    if(year%4==0 and year%100!=0 or year%400==0):
-        print("leap year")
-    else:
-        print("Not a leap year")    
+# QUES 7. CHECK LEAP YEAR.
 
-n=int(input())
-checkleap(n)
+# def checkleap(year):
+#     if(year%4==0 and year%100!=0 or year%400==0):
+#         print("leap year")
+#     else:
+#         print("Not a leap year")    
+
+# n=int(input())
+# checkleap(n)
+
+
+# QUES 8. FACTOR OF A NUMBER.
+
+# def factor(n):
+#         i=1
+#         while i<=n:
+#             if n%i==0:
+#                 print(i,end=' ')  
+#             i=i+1
+# n=int(input())
+# factor(n)
+
+
+# QUES 9. CHECK PALINDROME.
+
+# def cp(n):
+#     a=n
+#     rev =0
+#     while n>0:
+#         d=n%10
+#         rev = rev * 10 +d
+#         n=n//10
+#     if a==rev:
+#         print("palindrome")
+#     else:
+#         print("Not a palindrome")    
+
+# n=int(input())
+# cp(n)
+
+# QUES 10. CHECK ARMSTRONG
+
+# def arm(n):
+#     x=y=n
+#     digit = 0
+#     sum =0
+#     while n>0:
+#         digit = digit +1
+#         n=n//10
+
+
+#     while x>0:
+#         ld = x%10
+#         sum = sum +ld**digit
+#         x=x//10
+
+
+#     if y==sum:
+#         print(f'given no {y} is armstrong number.')
+#     else:
+#         print(f'given no {y} is not a armstrong.')        
+
+# n=int(input())
+# arm(n)
+
+
+# QUES 11. FIBONACCI SERIES.
+
+# def fib(n):
+#     x,y=0,1
+#     i=1
+#     print(x,end="," )
+#     print(y,end="," )
+#     while i<=n:
+#         z=x+y
+#         if i<n:
+#             print(z , end=',')
+#         else:
+#             print(z , end=' ')
+#         x,y=y,z
+#         i=i+1
+
+# n=int(input())
+# fib(n)
