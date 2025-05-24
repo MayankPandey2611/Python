508d58224c50e8d5b5a956ef76d28e2683be42d0
MayankPandey2611 <kitupnd@gmail.com>
2025-05-24 16:06:38 +0530

    oops concept

--- class_object.py
+++ class_object.py
@@ -31,29 +31,152 @@
 
 #--------------------------------------------INSTANCE VARIABLE DECLARATION AND ACCESS------------------------------------------------#
 
-class student:
-    def __init__(self , name , city):
-        self.n = name       #INSIDE CONSTRUCTOR (DECLARATION)
-        self.c = city
-        # print(self.n , self.c)   #ACCESS INSIDE CONSTRUCTOR.
+# class student:
+#     def __init__(self , name , city):
+#         self.n = name        #INSIDE CONSTRUCTOR (DECLARATION)
+#         self.c = city
+#         # print(self.n , self.c)   #ACCESS INSIDE CONSTRUCTOR.
 
-    def add(self,phone):
-        self.p = phone   #INSIDE INSTANCE METHOD (DECLARATION)
-        print(self.n , self.c , self.p , self.school)  # ACCESS INSIDE INSTANCE METHOD.
+#     def add(self,phone):
+#         self.p = phone    #INSIDE INSTANCE METHOD (DECLARATION)
+#         print(self.n , self.c , self.p , self.school)  # ACCESS INSIDE INSTANCE METHOD.
 
-obj = student('mayank' ,'bhopal')
+# obj = student('mayank' ,'bhopal')
 
-obj.school  = 'sirt'    #OUTSIDE OF THE CLASS (DECLARATION)
+# obj.school  = 'sirt'     #OUTSIDE OF THE CLASS (DECLARATION)
 
-obj.add(4567891234)
+# obj.add(4567891234)
 
-# print(obj.n , obj.c , obj.p , obj.school)  # ACCESS OUTSIDE THE CLASS
+# print(obj.n , obj.c , obj.p , obj.school)    # ACCESS OUTSIDE THE CLASS
 
+
+# QUESTION => ADD TWO NUMBERS IN CLASS.
+# class sum:
+#     def __init__(self,a,b):
+#         self.f=a
+#         self.s=b
+#         print(self.f+self.s)
+
+# obj = sum(12,10)
+
+
+# -------------------------------------------------------CLASS / STATIC METHODS---------------------------------------------------#
+
+# class student:
+#     school ='sirt'  #DECLARATION INSIDE THE CLASS BODY.
+#     def __init__(self,name):
+#         self.n = name
+    
+# obj = student('mayank')
+# obj1 = student('yash')
+# print(obj.n , obj.school)
+# print(obj1.n , obj1.school)
+
+
+# class student:
+#     school='sirt'     #DECLARATION INSIDE THE CLASS BODY.
+#     def __init__(self,name):
+#         self.n = name  
+#         student.code = 601      #DECLARATION INSIDE THE CONSTRUCTOR.
+#         print(student.school)
+#     def addnew(self):
+#         student.city = "bhopal"     #DECLARATION INSIDE INSTANCE METHOD.
+#         print(student.school , student.code , student.city)
+#     def show(self):
+#         print(student.grade)
+
+# student.grade  = '12'
+# obj = student('mayank')
+# obj.addnew()
+# obj.show()
+# print(obj.n)
+
+
+#-----------------------------------------------  LOCAL METHOD ---------------------------------------------#
+
+# class student:
+#     def __init__(self,name):
+#         grade='12'
+#         self.n = name
+#         print(grade)
+#     def new(self):
+#         print(student.grade)   # LOCAL VARIABLE CANNOT BE ACCESSED INSIDE INSTANCE METHOD.
+# obj  = student('mayank')
+# obj.new('12')
 
 # OBJECT
 # CONSTRUCTOR
 
 
+# METHODS => 1. INSTNCE
 
 
+#  2. CLASS METHODS
 
+# class student:
+#     school='sirt'
+#     grade='10th'
+#     def __init__(self,name):
+#         self.n=name
+#     def show(self):
+#         print(self.n)
+#         print(self.school)
+#         print(self.grade)
+#         print(self.city)
+#     @classmethod  # INBULIT DECORATER (@ adds the cls with student class)
+#     def update(cls,updated):
+#         cls.grade = updated
+
+#     @classmethod
+#     def add(cls,city):
+#         cls.city = city
+
+# obj = student('mayank')
+# # obj.show()
+
+# student.update('11th')
+
+# # obj2=student('ayush')
+# # obj2.show()
+
+# student.add('bhopal')
+# obj.show()
+
+
+#  3. STATIC METHODS
+
+
+# class student:
+#     school='sirt'
+#     grade='10th'
+#     def __init__(self,name):
+#         self.n=name
+#     def show(self):
+#         print(self.n)
+#         print(self.school)
+#         print(self.grade)
+        
+#     @staticmethod
+#     def add(cls,city):
+#         print(cls)
+#         print(city)
+
+# obj = student('mayank')
+# student.add('bhopal','rewa')
+# obj.show()
+
+
+# class greet:
+#     def __init__(self,name):
+#         self.n = name
+#     @staticmethod
+#     def welcome():
+#         print('welcome to our web page')
+
+#     @staticmethod
+#     def bye():
+#         print('Thanks for visting')
+
+# obj = greet("mayank")
+# obj.welcome()
+# obj.bye()
