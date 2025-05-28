ee36d24d26af8f56a1e46dadae8e234cf1662c51
MayankPandey2611 <kitupnd@gmail.com>
2025-05-28 18:26:11 +0530

    oops

--- oops.py
+++ oops.py
@@ -1,6 +1,6 @@
 
 # FOUR PROPERTIES =>
-# 1. INHERITANCE =>  USED TO INHERIT THE PARENT CLASS BY CHILD CLASS.
+# --------------------------------1. INHERITANCE =>  USED TO INHERIT THE PARENT CLASS BY CHILD CLASS.
 
 # TYPES => 
 
@@ -90,35 +90,148 @@
 
 # 4. HIERARICHICAL => PARENT ------. CHILD 1-------> CHILD 2
 
-class Parent:
-    x=10
-    def __init__(self,name):
-        self.n = name
-        print(id(self))
+# class Parent:
+#     x=10
+#     def __init__(self,name):
+#         self.n = name
+#         print(id(self))
 
-    def home(self):
-        print('home from parent class')
+#     def home(self):
+#         print('home from parent class')
 
-class Child1(Parent):
-    def home(self):
-        print('home from child class')
-        super().home()
-        # SUPER => USED TO OVERCOME METHOD OVERRIDDING . (IT PRINTS THE STATEMENT OF PARENT CLASS.)
-class Child2(Parent):
-    pass
-obj=Child1('mayank')
-obj1=Child2('ayush')
-print(obj.x)
-print(obj1.x)
-print(obj.n)
-print(obj1.n)
-obj.home()
-obj1.home()
-print(id(obj))
+# class Child1(Parent):
+#     def home(self):
+#         print('home from child class')
+#         super().home()
+#         # SUPER => USED TO OVERCOME METHOD OVERRIDDING . (IT PRINTS THE STATEMENT OF PARENT CLASS.)
+# class Child2(Parent):
+#     pass
+# obj=Child1('mayank')
+# obj1=Child2('ayush')
+# print(obj.x)
+# print(obj1.x)
+# print(obj.n)
+# print(obj1.n)
+# obj.home()
+# obj1.home()
+# print(id(obj))
 
-# 5. HYBRID 
+# 5. HYBRID  => 
+
+# class A :
+#     def first(self):
+#         print('from class A')
+# class B:
+#     def first(self):
+#         print('from class B')
+# class C(A):
+#     pass
+# class D(B):
+#     pass
+
+# class E(D,C):  #MRO => METHOD REDUCTION 
+#     pass
+
+# obj = E()
+# obj.first()
 
 
-# 2. ABSTRACTION
-# 3. POLYMORPHISM
-# 4. ENCAPSULATION
+
+# -----------------------------------------------2. ABSTRACTION => 
+# ABSTRACT CLASS => IF ABC IS IMPORT AND USES MORE THAN 2 METHODS IN CLASS ANS ATLEAST ONE ABSTRACT METHOD THEN IT IS CALLED AS ABSTRACT CLASS.
+#  , ABSTRACT METHOD , CANCREATE METHOD 
+# ABC = ABSTRUCT BASE CLASS
+
+# from abc import ABC , abstractmethod
+
+# class Senior(ABC):
+#     def add(self,x,y):
+#         print(x+y)
+#     def sub(self,x,y):
+#         print(x-y)
+#     def multi(self,x,y):
+#         print(x*y)
+#     @abstractmethod
+#     def div(self):
+#         pass
+# class Junior(Senior):
+#     def div(self,x,y):
+#         print(x/y)
+# obj= Junior()
+# obj.add(10,2)
+# obj.sub(10,2)
+# obj.multi(10,2)
+# obj.div(10,2)
+
+#-------------------------------------------3. POLYMORPHISM =>
+
+# METHOD OVERLOADING IS NOT SUPPORTED IN PYTHON. 
+
+# MULIPLY BEHAVIOER (MANY FORMS) OF A SINGLE FUNCTION IS POLYMORPHISM.
+
+
+class XYZ :
+    def __init__(self):
+        print('from 1st constructor')    
+    def __init__(self,x,y):
+        print('from 2nd constructor')
+obj = XYZ()
+
+# -------------------------------------------4. ENCAPSULATION =>
+
+#   MULTIPLY VARIABLES AND VALUES ARE KEPT IN A SINGLE CLASS THEN IT IS CALLED AS ENCAPSULATION.
+
+# ACCESS SPECIFER / MODIFIER =>  NOT RECOMMANDED BY PYHTON . 
+
+#  PUBLIC  (X / SHOW())
+
+# class P:
+#     x=10
+#     def show(self):
+#         print('from p class')
+# class C(P):
+#     pass
+# obj = C()
+# print(obj.x)
+# print(obj.show())
+
+
+# PROTECTED(PYTHON NOT SUPPORTED)  (_X / _SHOW())
+
+# class P:
+#     _x=10
+#     def _show(self):
+#         print('from p class')
+# class C(P):
+#     pass
+# obj = C()
+# print(obj._x)
+# print(obj._show())
+
+
+# PRIVATE  (__X / __SHOW )
+
+# class P:
+#     __x=10
+#     def __show(self):
+#         print('from p class')
+# class C(P):
+#     print(P.__x)
+# obj = C()
+# print(obj.__x)
+# print(obj.__show())
+
+
+# ALL THE CLASS CAN BE ACCESSABLE (DIR) .
+
+# class P:
+#     __x=10
+#     def __show(self):
+#         print('from p class')
+# class C(P):
+#     pass
+# obj = C()
+# # print(dir(C))
+# print(obj._P__x)
+# print(obj._P__show())
+
--- questions.py
+++ questions.py
@@ -1,6 +1,18 @@
 
-num=int(input('enter the number'))
+# num=int(input('enter the number'))
 
-ans=str(num)
-print(ans[0])
-print(ans[-1])
+# ans=str(num)
+# print(ans[0])
+# print(ans[-1])
+
+# QUES 1. ASCENTING ORDER.
+
+l=[2,1,3,4,1,3,5]
+
+
+for i in range(0,6):
+    if(i<i+1):
+        l[i],l[i+1]=l[i+1],l[i]
+     
+
+print(l)
